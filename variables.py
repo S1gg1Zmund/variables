a394a6b508f75942e03ad368ba6569c4d798115c
S1gg1Zmund <vadikovichvadik7@gmail.com>
2024-11-06 13:48:09 +0300

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,6 @@
+a = 'The_number_of_Complited_homework = 12'
+b = 'Number_of_hours_spent = 1.5'
+c = 'Course_name = "Python"'
+print('Number_of_hours_spent / The_number_of_Complited_homework')
+d = 'Time_for_one_task = 0.125'
+print(a, b, c, d)
